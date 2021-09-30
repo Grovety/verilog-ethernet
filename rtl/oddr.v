d6ade5d3ad21706fab98f02e17770ae270e77a4f
Sergey Turkin <agency@grovety.com>
2021-09-30 19:53:21 +0300

    Lattice support is added

--- rtl/oddr.v
+++ rtl/oddr.v
@@ -115,6 +115,39 @@ end else if (TARGET == "ALTERA") begin
         .aclr(1'b0),
         .dataout(q)
     );
+end else if (TARGET == "LATTICE") begin
+    for (n = 0; n < WIDTH; n = n + 1) begin : oddr
+/*         ODDR #(
+             .DDR_CLK_EDGE("SAME_EDGE"),
+             .SRTYPE("ASYNC")
+         )
+         oddr_inst (
+             .Q(q[n]),
+             .C(clk),
+             .CE(1'b1),
+             .D1(d1[n]),
+             .D2(d2[n]),
+             .R(1'b0),
+             .S(1'b0)
+         );*/
+         wire temp;
+         ODDRX1F ODDRX1F_2(
+	     .D0(d1[n]),
+	     .D1(d2[n]),
+	     .SCLK(clk),
+             .RST (1'b0),
+	     .Q(temp)
+         );
+
+         DELAYG #(
+	    .DEL_MODE("SCLK_ALIGNED"),
+	    .DEL_VALUE(1'd0)
+        ) DELAYG_2 (
+	   .A(temp),
+	   .Z(q[n])
+);
+
+     end 
 end else begin
     reg [WIDTH-1:0] d_reg_1 = {WIDTH{1'b0}};
     reg [WIDTH-1:0] d_reg_2 = {WIDTH{1'b0}};
