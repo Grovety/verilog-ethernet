251cc74582714cb7886d66c8ab8facadc697ea1c
Sergey Turkin <agency@grovety.com>
2021-12-07 01:42:41 +0300

    1G Capability Disabler is added for special "10/100 MBit Only" version

--- example/ColorLight 5A-75B/fpga_no1G/rtl/fpga.v
+++ example/ColorLight 5A-75B/fpga_no1G/rtl/fpga.v
@@ -12,7 +12,9 @@ module fpga #(
     output wire        eth_tx_ctl,
     output wire [3:0]  eth_tx_data,
     output             eth_mdc,
-    inout              eth_mdio
+    inout              eth_mdio,
+// For Debug Purposes Only!
+   output       [6:0]  test
 );
 
 localparam MAX = 12_500_000;
@@ -83,6 +85,116 @@ pll_i (
     .ENCLKOP(1'b0),
     .LOCK(locked)
 );
+
+// MDIO logic needs for disable 1G capability
+
+reg [15:0] delay_reg = 16'hffff;
+
+reg [4:0] mdio_cmd_phy_addr = 5'h00;
+reg [4:0] mdio_cmd_reg_addr = 5'h00;
+reg [15:0] mdio_cmd_data = 16'd0;
+reg [1:0] mdio_cmd_opcode = 2'b01;
+reg mdio_cmd_valid = 1'b0;
+wire mdio_cmd_ready;
+
+reg [3:0] mdio_state;
+
+always @(posedge clk_i) 
+begin
+    if (rst) 
+    begin
+        mdio_state <= 0;
+        delay_reg <= 16'hffff;
+        mdio_cmd_reg_addr <= 5'h00;
+        mdio_cmd_data <= 16'd0;
+        mdio_cmd_valid <= 1'b0;
+        mdio_cmd_opcode = 2'b01;
+    end else 
+    begin
+        mdio_cmd_valid <= mdio_cmd_valid & !mdio_cmd_ready;
+        if (delay_reg > 0) 
+        begin
+            delay_reg <= delay_reg - 1;
+        end else if (!mdio_cmd_ready) 
+        begin
+            // wait for ready
+            mdio_state <= mdio_state;
+        end else 
+        begin
+            mdio_cmd_valid <= 1'b0;
+            case (mdio_state)
+                // set SGMII autonegotiation timer to 11 ms
+                // write 0x0070 to CFG4 (0x0031)
+                4'd0: 
+                begin
+                    // Disable 1000M Capability
+                    mdio_cmd_reg_addr <= 5'h09;
+                    mdio_cmd_data <= 16'h0000;
+                    mdio_cmd_valid <= 1'b1;
+                    mdio_state <= 4'd1;
+                end
+                4'd1: 
+                begin
+                    // Re Negotiate
+                    mdio_cmd_reg_addr <= 5'h00;
+                    mdio_cmd_data <= 16'h1340;
+                    mdio_cmd_valid <= 1'b1;
+                    mdio_state <= 4'd12;
+                end
+                // ...
+                // Values from 1 to 11 are reserved
+                // ...
+                4'd12: begin
+                    // done
+                    mdio_state <= 4'd12;
+                end
+            endcase
+        end
+    end
+end
+
+wire mdc;
+wire mdio_i;
+wire mdio_o;
+wire mdio_t;
+
+mdio_master
+mdio_master_inst (
+    .clk(clk_i),
+    .rst(rst),
+
+    .cmd_phy_addr(mdio_cmd_phy_addr),
+    .cmd_reg_addr(mdio_cmd_reg_addr),
+    .cmd_data(mdio_cmd_data),
+    .cmd_opcode(mdio_cmd_opcode),
+    .cmd_valid(mdio_cmd_valid),
+    .cmd_ready(mdio_cmd_ready),
+
+    .data_out(),
+    .data_out_valid(),
+    .data_out_ready(1'b1),
+
+    .mdc_o(mdc),
+    .mdio_i(mdio_i),
+    .mdio_o(mdio_o),
+    .mdio_t(mdio_t),
+
+    .busy(),
+
+    .prescale(8'd3)
+);
+
+assign eth_mdc = mdc;
+assign mdio_i = eth_mdio;
+assign eth_mdio = mdio_t ? 1'bz : mdio_o;
+
+assign test [0] = mdc;
+assign test [1] = mdio_i;
+assign test [2] = mdio_o;
+assign test [3] = mdio_t;
+assign test [4] = mdio_cmd_valid;
+assign test [5] = mdio_cmd_ready;
+assign test [6] = mdio_cmd_valid;
 
 fpga_core #(
     .TARGET(TARGET),
