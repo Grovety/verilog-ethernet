2c7757434bba4bc41be407880a81c5e1e2dcbaaf
Anatoly Selivanov <Anatoly.Selivanov@mir.dev>
2021-12-10 14:47:58 +0300

    Generation script for Yosys by Makefile is implemented

--- example/ColorLight 5A-75B/fpga/common/lattice.mk
+++ example/ColorLight 5A-75B/fpga/common/lattice.mk
@@ -29,6 +29,7 @@
 BOARD:=pinout_v7
 
 SYN_FILES_REL = $(patsubst %, ../%, $(SYN_FILES))
+FILES_TO_SCRIPT = $(patsubst %.sv, "-sv %.sv", $(SYN_FILES_REL))
 
 ###################################################################
 # Main Targets
@@ -48,8 +49,18 @@ clean:
 # Target implementations
 ###################################################################
 
-$(FPGA_TOP).json: $(SYN_FILES_REL)
-	yosys -p "synth_ecp5 -top $(FPGA_TOP) -json $@ -abc2" $(SYN_FILES_REL)
+$(FPGA_TOP).json: $(FPGA_TOP).ys $(SYN_FILES_REL)
+	yosys $(FPGA_TOP).ys
+
+$(FPGA_TOP).ys:
+	echo "verilog_defaults -push" > $(FPGA_TOP).ys
+	echo "verilog_defaults -add -defer" >> $(FPGA_TOP).ys
+	for file in $(FILES_TO_SCRIPT); do \
+		echo "read_verilog $$file" >> $(FPGA_TOP).ys; \
+	done
+	echo "verilog_defaults -pop" >> $(FPGA_TOP).ys
+	echo "attrmap -tocase keep -imap keep="true" keep=1 -imap keep="false" keep=0 -remove keep=0" >> $(FPGA_TOP).ys
+	echo "synth_ecp5 -top fpga -json fpga.json -abc9" >> $(FPGA_TOP).ys
 
 $(FPGA_TOP)_out.config: $(FPGA_TOP).json
 	nextpnr-ecp5 --25k --package CABGA256 --speed 6 --json $< --textcfg $@ --lpf ../$(BOARD).lpf --freq 166
